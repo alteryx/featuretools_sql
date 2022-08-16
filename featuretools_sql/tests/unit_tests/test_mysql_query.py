ddaae37aa1f029074a58e7a0d9054b29ad30aecc
sbadithe <slyokapi@gmail.com>
2022-08-16 12:48:03 -0700

    Cleanup

--- featuretools_sql/tests/unit_tests/test_mysql_query.py
+++ featuretools_sql/tests/unit_tests/test_mysql_query.py
@@ -49,7 +49,6 @@ def test_can_get_all_tables(mysql_connection):
 def test_can_learn_dataframes(mysql_connection):
     c = DBConnector(**mysql_connection)
     c.populate_dataframes(debug=False)
-    assert len(c.dataframes) > 0 
     es = EntitySet("es", c.dataframes, [])
     assert es is not None
 
