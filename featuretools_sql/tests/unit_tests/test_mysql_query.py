0d8b23dec7b4f9223d91a4b33490c68ec3116725
Gaurav Sheni <gvsheni@gmail.com>
2022-08-22 17:32:46 -0400

    Update featuretools_sql/tests/unit_tests/test_mysql_query.py

--- featuretools_sql/tests/unit_tests/test_mysql_query.py
+++ featuretools_sql/tests/unit_tests/test_mysql_query.py
@@ -22,7 +22,7 @@ def mysql_connection():
     config["port"] = "3306"
     config["password"] = "harrypotter"
     config["user"] = "root"
-    config["database"] = "postgres"
+    config["database"] = "dummy"
     return config
 
 
