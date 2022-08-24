b387f02a59ba997f506e272eb88cef7d9e227c6b
sbadithe <slyokapi@gmail.com>
2022-08-24 11:55:21 -0700

    Debugv2

--- featuretools_sql/db_connectors/mysql_connector.py
+++ featuretools_sql/db_connectors/mysql_connector.py
@@ -20,9 +20,9 @@ class MySQLConnector:
         self.relationships = []
 
     def all_tables(self, select_only=None) -> pd.DataFrame:
-        print(f"Select only: {select_only}")
         if isinstance(select_only, list):
             select_only_tables = ", ".join([f"'{i}'" for i in select_only])
+            print(f"Select only: {select_only_tables}")
             return self.run_query(
                 f"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{self.database}' AND TABLE_NAME IN ({select_only_tables});",
             )
