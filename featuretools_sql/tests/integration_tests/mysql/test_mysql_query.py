58905a65273e3d9e256d08e8567e3b01399baf30
sbadithe <slyokapi@gmail.com>
2022-08-24 12:00:46 -0700

    does caps lock matter

--- featuretools_sql/tests/integration_tests/mysql/test_mysql_query.py
+++ featuretools_sql/tests/integration_tests/mysql/test_mysql_query.py
@@ -41,7 +41,7 @@ def test_can_learn_dataframes(mysql_connection):
 
 def test_can_learn_dataframes_and_relationships(mysql_connection):
     sql_connection = DBConnector(**mysql_connection)
-    sql_connection.populate_dataframes(select_only=["products", "transactions"])
+    sql_connection.populate_dataframes(select_only=["PRODUCTS", "TRANSACTIONS"])
     print(f"Dataframes: {sql_connection.dataframes}")
     sql_connection.populate_relationships()
     es = EntitySet("es", sql_connection.dataframes, sql_connection.relationships)
