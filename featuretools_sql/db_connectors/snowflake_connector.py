98812414e4a124b23f59c404fc1445dde50bff1a
sbadithe <shripad.badithe@alteryx.com>
2022-10-03 17:29:12 -0700

    Update relationships query

--- featuretools_sql/db_connectors/snowflake_connector.py
+++ featuretools_sql/db_connectors/snowflake_connector.py
@@ -37,7 +37,6 @@ class SnowflakeConnector:
         select_only=None,
     ) -> Dict[str, Tuple[pd.DataFrame, str]]:
         tables_df = self.all_tables(select_only)
-        print(f"tables_df: {tables_df}")
         table_index = "table_name"
         self.tables = []
         dataframes = dict()
@@ -45,51 +44,31 @@ class SnowflakeConnector:
             self.tables.append(table)
             table_df = self.get_table(table)
             table_key = self.get_primary_key_from_table(table).values[0]
-            dataframes[table] = (table_df, table_key)
-        print(f"Dataframes: {dataframes}")
+            dataframes[table] = (table_df, table_key.lower())
         return dataframes
 
     def get_table(self, table: str) -> pd.DataFrame:
         return self.run_query(f"SELECT * FROM {self.database}.{self.schema}.{table}")
 
     def populate_relationships(self) -> List[Tuple[str, str, str, str]]:
-        query_str = """
-            select kcu.table_schema || '.' || kcu.table_name as foreign_table,
-            rel_kcu.table_schema || '.' || rel_kcu.table_name as primary_table,
-            kcu.column_name as fk_column,
-            rel_kcu.column_name as pk_column
-            from information_schema.table_constraints tco
-            join information_schema.key_column_usage kcu
-            on tco.constraint_schema = kcu.constraint_schema
-            and tco.constraint_name = kcu.constraint_name
-            join information_schema.referential_constraints rco
-            on tco.constraint_schema = rco.constraint_schema
-            and tco.constraint_name = rco.constraint_name
-            join information_schema.key_column_usage rel_kcu
-            on rco.unique_constraint_schema = rel_kcu.constraint_schema
-            and rco.unique_constraint_name = rel_kcu.constraint_name
-            and kcu.ordinal_position = rel_kcu.ordinal_position
-            where tco.constraint_type = 'FOREIGN KEY'
-            order by kcu.table_schema,
-            kcu.table_name,
-            kcu.ordinal_position;
-            """
         relationships = []
-        foreign_keys = self.run_query(query_str)
-        if self.system_name == "snowflake":
-            for (
-                foreign_table,
-                primary_table,
-                foreign_col,
-                primary_col,
-            ) in foreign_keys.values:
-                if "." in foreign_table:
-                    foreign_table = self.__cut_schema_name(foreign_table)
-                if "." in primary_table:
-                    primary_table = self.__cut_schema_name(primary_table)
-                if foreign_table in self.tables and primary_table in self.tables:
-                    r = (primary_table, primary_col, foreign_table, foreign_col)
-                    relationships.append(r)
+        foreign_keys = self.run_query("SHOW IMPORTED KEYS;")
+        for (
+            _, _, _,
+            primary_table,
+            primary_col,
+            _, _,
+            foreign_table,
+            foreign_col,
+            _, _, _, _, _, _, _, _
+        ) in foreign_keys.values:
+            if "." in foreign_table:
+                foreign_table = self.__cut_schema_name(foreign_table)
+            if "." in primary_table:
+                primary_table = self.__cut_schema_name(primary_table)
+            if foreign_table in self.tables and primary_table in self.tables:
+                r = (primary_table, primary_col.lower(), foreign_table, foreign_col.lower())
+                relationships.append(r)
         return relationships
 
     def get_primary_key_from_table(self, table: str) -> pd.DataFrame:
